1569234e046910921135032c1d11bb83a726b28f
gvang146 <gvang146@gmail.com>
2022-02-28 12:28:29 -0600

    signup added

--- ephoneshop/src/app/app-routing.module.ts
+++ ephoneshop/src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { ProductsComponent } from './products/products.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { DeveloperComponent } from './developer/developer.component';
+import { SignupComponent } from './signup/signup.component';
+
 
 const routes: Routes = [
   { path: 'products', component: ProductsComponent},
   { path: 'login', component: LoginComponent},
   { path: 'developer', component: DeveloperComponent},
   { path: '', component: HomeComponent},
+  { path: 'signup', component:SignupComponent}
 ];
 
 @NgModule({
@@ -17,4 +20,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const RoutingComponents = [ProductsComponent, LoginComponent, HomeComponent, DeveloperComponent];
+export const RoutingComponents = [ProductsComponent, LoginComponent, HomeComponent, DeveloperComponent, SignupComponent];
