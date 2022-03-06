3a0f6f8b70d892390350b20d1542cde47e61f9b9
LordLark <31074580+LordLark@users.noreply.github.com>
2022-03-06 18:19:01 -0500

    Contact Form is being read
    The contact form on the homepage recieves the users input and prompts them their info has been recieved.

--- ephoneshop/src/app/home/home.component.ts
+++ ephoneshop/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EphoneAPIService } from '../ephone-api.service';
 
 declare var $: any;
 declare var responsive_menu: any;
@@ -7,17 +8,27 @@ declare var responsive_menu: any;
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
-  
+
 })
 
 
-export class HomeComponent  implements OnInit {  
-  name = 'Jquery Integration With Angular!';  
-  isJqueryWorking: any;  
-  ngOnInit()  
-  {  
-    
+export class HomeComponent  implements OnInit {
+  name = 'Jquery Integration With Angular!';
+  isJqueryWorking: any;
+  ngOnInit()
+  {
 
-  }  
 
-}   
+  }
+   constructor(private service: EphoneAPIService)
+  {}
+
+/*uses the contructor service varialbe that inherits the EphonAPI to send the info retrieved from the
+ form*/
+  getUserContactInfo(info: any) {
+    this.service.postContactInfo(info);
+    console.warn(info);
+    alert("We have recieved your message" + info.Name);
+  }
+
+}
