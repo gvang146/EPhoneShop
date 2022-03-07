9958bcc6baeb0a4e1a3d0362b4f2151ed2215a1b
gvang146 <gvang146@gmail.com>
2022-03-06 20:10:13 -0600

    updates authenticated response

--- ephoneshopapi/EPhoneApi/Models/AuthenticateResponse.cs
+++ ephoneshopapi/EPhoneApi/Models/AuthenticateResponse.cs
@@ -1,11 +1,19 @@
+using EPhoneApi.Entities;
+
 namespace EPhoneApi.Models;
 
 public class AuthenticateResponse
 {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
     public string Token { get; set; }
 
-    public AuthenticateResponse(string token)
+    public AuthenticateResponse(UserEntity userEntity, string token)
     {
+        FirstName = userEntity.FirstName;
+        LastName = userEntity.LastName;
+        Email = userEntity.Email;
         Token = token;
     }
 }
