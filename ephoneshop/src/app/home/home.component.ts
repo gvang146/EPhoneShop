0c83f42e1e7804e6114161d2b1cbc69ceaaf0b59
LordLark <31074580+LordLark@users.noreply.github.com>
2022-03-07 15:14:35 -0500

    Redone with initially no issues
    The prompt was undefined. Went back and pulled from the finishedLogin branch to start over and save the initial code.

--- ephoneshop/src/app/home/home.component.ts
+++ ephoneshop/src/app/home/home.component.ts
@@ -7,17 +7,23 @@ declare var responsive_menu: any;
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
-  
+
 })
 
 
-export class HomeComponent  implements OnInit {  
-  name = 'Jquery Integration With Angular!';  
-  isJqueryWorking: any;  
-  ngOnInit()  
-  {  
-    
+export class HomeComponent  implements OnInit {
+  name = 'Jquery Integration With Angular!';
+  isJqueryWorking: any;
+  ngOnInit()
+  {
 
-  }  
 
-}   
+  }
+
+   getUserContactInfo(info: any) {
+    //this.service.postContactInfo(info);
+    console.warn(info);
+    alert("We have recieved your message " + info.Name);
+  }
+
+}
