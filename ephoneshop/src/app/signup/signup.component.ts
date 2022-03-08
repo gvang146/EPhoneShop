5ebc6274ddf4c225962a057f33685434f7f34956
Gitmemain <99740007+Gitmemain@users.noreply.github.com>
2022-03-08 10:31:54 -0500

    feb8brandon

--- ephoneshop/src/app/signup/signup.component.ts
+++ ephoneshop/src/app/signup/signup.component.ts
@@ -5,7 +5,7 @@ import { SignupService } from '../_services/SignupService.service';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-signup',
-  templateUrl: './signup.component.html',
+  templateUrl: 'signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
